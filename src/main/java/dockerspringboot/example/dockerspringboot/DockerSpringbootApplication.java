548496cf7315418ef957b8d54f839a1501b9d68f
jeral <jerald19960606@gmail.com>
2023-06-01 14:06:54 +0530

    first commit

---
+++ src/main/java/dockerspringboot/example/dockerspringboot/DockerSpringbootApplication.java
@@ -0,0 +1,20 @@
+package dockerspringboot.example.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerSpringbootApplication {
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hii i am jerald...!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringbootApplication.class, args);
+	}
+
+}
